3a8a0e26cb60d9de01f88711bf32adb9b3a6ca49
Javier Flores <jffc.dev@gmail.com>
2026-06-10 13:23:19 -0500

    feat: update Gmail authentication to use OAuth2 and remove app password

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ let browser
 let browserPromise
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    service: 'gmail',
     auth: {
+        type: 'OAuth2',
         user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD,
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
     },
 })
 
